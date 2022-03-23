12abcad196b5b8e67d3b955c45cb2197b85b3107
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-03-23 11:40:16 +0100

    Add more testing stuff

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 import express from "express";
 const app = express();
 
-let testRoutes = require('./routes/testrouter.route');
+let testRoutes = require("./routes/testrouter.route");
+app.use(express.json());
 app.use("/tests", testRoutes);
-
-
 
 async function main() {
   app.get("/", (req, res) => {
     res.send("Hello World!");
   });
-app.get("/api/", (req, res) => {
-  res.send("  welcome World running on port 3000");
-})
+
   app.listen(3000, () => {
     console.log("Server is running on port 3000");
   });
--- src/routes/testrouter.route.ts
+++ src/routes/testrouter.route.ts
@@ -1,10 +1,13 @@
-import { Request, Response } from "express";
 
-const express = require('express');
+import express, { Request, Response } from "express";
+import { sayHello } from "../controller/testcontroller.controller";
+
 const router = express.Router();
- 
-router.get('/', (request: Express.Request, response: Response) => {
+
+router.get("/", (request: Request, response: Response) => {
   response.status(200).send("Cool test stuff!");
 });
+
+router.post("/test1/:id", sayHello);
 
 module.exports = router;
