cb62400f390d22e0170d1a36271f932e4f229d47
Stone_Red <56473591+Stone-Red-Code@users.noreply.github.com>
2022-03-30 12:21:47 +0200

    Add restaurant and organization controller - Add restaurant controller - Add organization controller - Add files for types

--- src/controller/testcontroller.controller.ts
+++ src/controller/testcontroller.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-export function sayHello(request: Request, response: Response) {
-    console.log(request.params);
-    response.send(request.params.id);
+export const sayHello = async (request: Request, response: Response) => 
+{
+    response.status(200).send("Hello!");
 }
--- src/routes/testrouter.route.ts
+++ src/routes/testrouter.route.ts
@@ -1,6 +1,6 @@
-
 import express, { Request, Response } from "express";
 import { sayHello } from "../controller/testcontroller.controller";
+import { addRestaurant } from "../controller/restaurants.controller";
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ router.get("/", (request: Request, response: Response) => {
   response.status(200).send("Cool test stuff!");
 });
 
-router.post("/test1/:id", sayHello);
+router.post("/test1", sayHello);
+router.post("/test2", addRestaurant);
 
 module.exports = router;
